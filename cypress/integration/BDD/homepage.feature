4a39657fd43b676e8dc650f8e66dfb76033c7d60
MKhadijatO <muhammadkhadijato@gmail.com>
2024-10-02 16:26:45 +0100

    set-up

---
+++ cypress/integration/BDD/homepage.feature
@@ -0,0 +1,9 @@
+Feature: MaxTech End-to-end Test
+ Application regression
+
+@regression
+
+Scenario: Validate Homepage screen
+    Given I open the website
+    When I scroll
+    Then I click
---
+++ cypress/integration/BDD/homepage/homepage.js
@@ -0,0 +1,4 @@
+/// <reference types="cypress" />
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe';
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
