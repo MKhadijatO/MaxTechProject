c63dfc6f67593802c22e08f9a925de6811c19b29
MKhadijatO <muhammadkhadijato@gmail.com>
2024-10-16 21:49:20 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -9,19 +9,46 @@
 // });
 
 
-const { defineConfig } = require("cypress");
-const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+// const { defineConfig } = require("cypress");
+// const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+// const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
-async function setupNodeEvents(on, config) {
-await preprocessor.addCucumberPreprocessorPlugin(on, config);
-on("file:preprocessor", browserify.default(config));
-return config;
-}
+// async function setupNodeEvents(on, config) {
+// await preprocessor.addCucumberPreprocessorPlugin(on, config);
+// on("file:preprocessor", browserify.default(config));
+// return config;
+// };
+
+// module.exports = defineConfig({
+// e2e: {
+//     setupNodeEvents,
+//     specPattern: 'cypress/integration/BDD/*.feature',
+// },
+// });
+
+
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const addCucumberPreprocessorPlugin = require('@badeball/cypress-cucumber-preprocessor').addCucumberPreprocessorPlugin;
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild').createEsbuildPlugin;
 
 module.exports = defineConfig({
-e2e: {
-setupNodeEvents,
-specPattern: 'cypress/integration/BDD/*.feature',
-},
+  e2e: {
+    async setupNodeEvents(on, config) {
+      // Add cucumber preprocessor plugin
+      await addCucumberPreprocessorPlugin(on, config);
+
+      // Add esbuild plugin for bundling
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
+    specPattern: '**/*.feature', // Adjust your path to where feature files are stored
+  },
 });
+
--- cypress/integration/BDD/homepage/homepage.js
+++ cypress/integration/BDD/homepage/homepage.js
@@ -17,25 +17,32 @@ Then ("I validate the navbar", function () {
 });
 
 Then ("I click on Blog and validate page open", function () {
-    cy.get("").should("");
+    cy.get("li[id='menu-item-17651'] a").click();
+    cy.url().should("include", "https://www.mtechitng.com/blog/");
 });
 
 Then ("I click on Learn and validate page open", function () {
-
+    cy.get("li[id='menu-item-22011'] a[target='_blank']").click();
+    cy.url().should("include", "https://www.learn.mtechitng.com/");
 });
 
 Then ("I click on Services and validate page open", function () {
-
+    cy.get("li[id='menu-item-17634'] a").click();
+    cy.url().should("include", "https://www.mtechitng.com/our-services/").should("contain.text", "HOME / SERVICES");
 });
 
 Then ("I click on Get A Quote and validate page open", function () {
-
+    cy.get("a[class='sf-with-ul']").click();
+    cy.get("li[id='menu-item-22168'] a").click();
+    cy.url().should("include", "https://www.mtechitng.com/device-repair-form/");
 });
 
 Then ("I click on Web development and validate page open", function () {
-
+    cy.get("li[id='menu-item-21443'] span").click();
+    cy.url().should("include", "https://web.mtechitng.com/");
 });
 
 When ("I validate Close button and then click on the icon", function () {
-    
+    cy.get("li[id='menu-item-20623'] span").click();
+    cy.url().should("include", "https://store.mtechitng.com/");
 });
